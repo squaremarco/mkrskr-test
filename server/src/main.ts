0f17f81ae9ed039d6d79e94a298d36c0ee068e73
Marco Cesi <squaremarco@gmail.com>
2022-03-18 13:03:36 +0100

    Add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,16 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(port, () =>
